d3dba0b296c6d9ee8535948b9c5edef03ced548b
Shaun McCormick <shaun@modx.com>
2013-06-19 15:11:54 -0500

    add website

--- lib/splittingred/rake.rb
+++ lib/splittingred/rake.rb
@@ -1,8 +1,8 @@
 require 'launchy'
 
 namespace :splittingred do
-  desc 'Launch my GitHub page, Twitter profile, LinkedIn profile and resume.'
-  task :all => [:github,:twitter,:linkedin,:resume]
+  desc 'Launch my personal website, GitHub page, Twitter profile, LinkedIn profile and resume.'
+  task :all => [:website,:github,:twitter,:linkedin,:resume]
 
   desc 'Launch my GitHub page.'
   task :github do
--- lib/splittingred/version.rb
+++ lib/splittingred/version.rb
@@ -1,3 +1,3 @@
 module Splittingred
-  VERSION = '0.1.2'
+  VERSION = '0.1.3'
 end
